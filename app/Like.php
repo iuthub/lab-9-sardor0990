aa470d0538d08dbeaa08bd8ffcd225561c063b74
sardor0990 <sardorzufarov98@gmail.com>
2020-04-14 16:32:11 +0300

    finished labmy9

---
+++ app/Like.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+namespace App;
+
+
+
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Like extends Model
+
+{
+
+    public function post()
+
+    {
+
+        return $this->belongsTo('App\Post', 'post_id');
+
+    }
+
+}
